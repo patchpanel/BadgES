d2775652235f646f28e110ead182023b2efe68be
Patch Panel <patchpanel@gmail.com>
2018-12-04 16:06:30 +0800

    Initial Commit

---
+++ BadgES.Module/BusinessObjects/BadgESDataModelCode/Summary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+
+namespace BadgES.Module.BusinessObjects.BadgES
+{
+    [DefaultClassOptions]
+    public partial class Summary
+    {
+        public Summary(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
